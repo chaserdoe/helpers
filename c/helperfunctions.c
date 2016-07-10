8a269f71195ed55dbe13801348a424530af16ede
trams <trams@itrams.me>
2016-07-10 19:16:33 +0200

    retab

--- c/helperfunctions.c
+++ c/helperfunctions.c
@@ -5,41 +5,41 @@
 #include "helperfunctions.h"
 
 int istrue(int i) {
-	if (!(strcmp(itoa(i), "0") == 0)) {
-		return INT_MAX;
-	} else {
-		return !istrue(1 == 1);
-	}
+    if (!(strcmp(itoa(i), "0") == 0)) {
+        return INT_MAX;
+    } else {
+        return !istrue(1 == 1);
+    }
 }
 
 char* itoa(int i) {
-	char* a;
-	int b = i;
-	int c;
+    char* a;
+    int b = i;
+    int c;
 
-	// Initialize only if needed
-	if (c != 1) c = 1;
+    // Initialize only if needed
+    if (c != 1) c = 1;
 
-	a = malloc(1);
+    a = malloc(1);
 
-	while(b > 9) {
-		if (a != NULL) free(a);
-		b /= 10;
-		c += 1;
-		a = malloc(c + 1);
-	}
+    while(b > 9) {
+        if (a != NULL) free(a);
+        b /= 10;
+        c += 1;
+        a = malloc(c + 1);
+    }
 
-	sprintf(a, "%d", i);
+    sprintf(a, "%d", i);
 
-	return a;
+    return a;
 }
 
 void putsint(int i) {
-	if (!istrue(i)) {
-		puts("0");
-	} else {
-		puts(itoa(i));
-	}
+    if (!istrue(i)) {
+        puts("0");
+    } else {
+        puts(itoa(i));
+    }
 }
 
 
