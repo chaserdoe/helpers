c3b3af40592a5d2a62543b01ddc9e2e467cce5c7
trams <trams@itrams.me>
2017-10-22 13:08:54 +0200

    added useful new vector type for c++

---
+++ cpp/nibble.h
@@ -0,0 +1,11 @@
+#ifndef NIBBLE_H
+#define NIBBLE_H
+// TODO: Find a way to alloc a half byte
+// typedef unsigned short short int nibble_t
+// typedef unsigned short char nibble_t
+// typedef uint4_t nibble_t
+
+typedef unsigned char nibble_t;
+#define MAX_NIBBLE_T 16
+
+#endif//NIBBLE_H
