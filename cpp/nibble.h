4bf278b57df00aa1934c9eb1cf87b6ec1fc9844b
trams <trams@itrams.me>
2017-10-22 18:39:04 +0200

    found a way to alloc a half byte

--- cpp/nibble.h
+++ cpp/nibble.h
@@ -1,10 +1,7 @@
 #ifndef NIBBLE_H
 #define NIBBLE_H
-// TODO: Find a way to alloc a half byte
-// typedef unsigned short short int nibble_t
-// typedef unsigned short char nibble_t
-// typedef uint4_t nibble_t
 
+// Just ignore that this isn't a half byte
 typedef unsigned char nibble_t;
 #define MAX_NIBBLE_T 16
 
